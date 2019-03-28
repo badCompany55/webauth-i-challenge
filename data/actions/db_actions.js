8d25d54c15fbbe77e817160ef964be54da5b0a6b
badCompany55 <zachirvin45@gmail.com>
2019-03-27 22:16:17 -0500

    LOGIN / USERS add login endpoint with authorization add users endpoint

--- data/actions/db_actions.js
+++ data/actions/db_actions.js
@@ -1,9 +1,21 @@
 const db = require("../knex.js");
 
 module.exports = {
-  add_user
+  add_user,
+  all_users,
+  single_user
 };
 
 function add_user(user) {
   return db("users").insert(user);
+}
+
+function all_users() {
+  return db("users");
+}
+
+function single_user(username) {
+  return db("users")
+    .where("use_name", username)
+    .first();
 }
--- routes/nonUsersRouter.js
+++ routes/nonUsersRouter.js
@@ -4,6 +4,20 @@ const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
+async function userCheck(req, res, next) {
+  const { use_name, pass_word } = req.body;
+  try {
+    const user = await db.single_user(use_name);
+    if (user && bcrypt.compareSync(pass_word, user.pass_word)) {
+      next();
+    } else {
+      res.status(401).json({ Error: "You shall not pass!!" });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
 router.post("/registar", async (req, res) => {
   let credentials = req.body;
   if (credentials.use_name && credentials.pass_word) {
@@ -20,17 +34,28 @@ router.post("/registar", async (req, res) => {
       res.status(201).json(user);
     } catch (err) {
       if ((err.errno = 19)) {
-        res
-          .status(400)
-          .json({
-            Error: "The username has already been taken. Please try another"
-          });
+        res.status(400).json({
+          Error: "The username has already been taken. Please try another"
+        });
       } else {
         res.status(500).json(err);
       }
     }
   } else {
     res.status(400).json({ Error: "Name and password are reqired" });
+  }
+});
+
+router.post("/login", userCheck, async (req, res) => {
+  res.status(200).json({ Message: "Logged In" });
+});
+
+router.get("/users", userCheck, async (req, res) => {
+  try {
+    const users = await db.all_users();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
